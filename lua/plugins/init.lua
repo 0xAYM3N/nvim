9b84dbb41bdbfbc7abba908def93d7157e39e891
0xAYM3N <aymen.dev10@gmail.com>
2025-09-27 20:48:47 +0100

    style(telescope): apply ivy theme, disable preview, adjust find_files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,22 @@ return {
       require("ccc").setup({})
     end,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts.defaults = require("telescope.themes").get_ivy({
+        layout_config = { height = 0.40 },
+        preview = false,
+        results_title = "",
+      })
+
+      opts.pickers = {
+        find_files = {
+          prompt_title = "",
+        },
+      }
+    end,
+  }
 
   -- toggleterm plugin
   -- test new blink
